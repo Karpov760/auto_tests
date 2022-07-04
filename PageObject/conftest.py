879c88e741ebbefb16eb3c5e5e54a3d85aa6cfd0
unknown <glintrom@gmail.com>
2022-07-04 17:23:17 +0500

    add conf for chose language

---
+++ PageObject/conftest.py
@@ -0,0 +1,33 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+
+
+
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default="en",
+                     help="Choose language: ru or en")
+
+
+@pytest.fixture(scope="function")
+def browser(request):
+    language = request.config.getoption("language")
+    browser = None
+    if language == "ru":
+        print("\nstart ru browser for test..")
+        options = Options()
+        options.add_experimental_option('prefs', {'intl.accept_languages': "ru"})
+        browser = webdriver.Chrome(options=options)
+    elif language == "en":
+        print("\nstart en browser for test..")
+        options = Options()
+        options.add_experimental_option('prefs', {'intl.accept_languages': "en"})
+        browser = webdriver.Chrome(options=options)
+    else:
+        raise pytest.UsageError("--browser_name should be chrome or firefox")
+    print("\nstart browser for test..")
+
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
