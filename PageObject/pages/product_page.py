a69636609ee27c95a43a1f30fa30c5ca5afdd55b
unknown <glintrom@gmail.com>
2022-07-06 15:27:38 +0500

    add negative tests

--- PageObject/pages/product_page.py
+++ PageObject/pages/product_page.py
@@ -8,6 +8,13 @@ class ProductPage(BasePage):
         add_button = self.browser.find_element(*ProductPageLocators.ADD_BUTTON)
         add_button.click()
 
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.ADDED_NAME), "Success message is presented, but should not be"
+
+    def should_not_be_success_message_dis(self):
+        assert self.is_disappeared(*ProductPageLocators.ADDED_NAME), "Success message is presented, but should not be"
+
+
     def should_be_same_names(self):
         assert self.get_element_text(*ProductPageLocators.NAME) == self.get_element_text(*ProductPageLocators.ADDED_NAME), "Names are not the same"
 
--- PageObject/test_product_page.py
+++ PageObject/test_product_page.py
@@ -7,6 +7,9 @@ product_base_link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-w
 urls = [f"{product_base_link}/?promo=offer{no}" for no in range(10)]
 
 
+
+
+@pytest.mark.skip
 @pytest.mark.parametrize('link', urls)
 def test_guest_can_add_product_to_basket(browser, link):
     page = ProductPage(browser, link)
@@ -15,3 +18,22 @@ def test_guest_can_add_product_to_basket(browser, link):
     page.solve_quiz_and_get_code()
     page.should_be_same_names()
     page.should_be_same_prices()
+
+@pytest.mark.xfail
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    page = ProductPage(browser, product_base_link)
+    page.open()
+    page.add_product_to_basket()
+    page.should_not_be_success_message()
+
+
+def test_guest_cant_see_success_message(browser):
+    page = ProductPage(browser, product_base_link)
+    page.open()
+    page.should_not_be_success_message()
+
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    page = ProductPage(browser, product_base_link)
+    page.open()
+    page.add_product_to_basket()
+    page.should_not_be_success_message_dis()
