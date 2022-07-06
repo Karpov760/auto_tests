3c454409b14fc58f2bd988d91f3663068ccbcf9e
unknown <glintrom@gmail.com>
2022-07-06 14:41:28 +0500

    add parametrs

--- PageObject/test_product_page.py
+++ PageObject/test_product_page.py
@@ -1,11 +1,14 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from .pages.product_page import ProductPage
+import pytest
 
-link = "http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+product_base_link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
+urls = [f"{product_base_link}/?promo=offer{no}" for no in range(10)]
 
 
-def test_guest_can_add_product_to_basket(browser):
+@pytest.mark.parametrize('link', urls)
+def test_guest_can_add_product_to_basket(browser, link):
     page = ProductPage(browser, link)
     page.open()
     page.add_product_to_basket()
